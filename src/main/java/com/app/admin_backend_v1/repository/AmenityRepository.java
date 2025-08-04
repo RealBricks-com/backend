844028f62e73dc19efad963bb2e535e74ca37246
omkar kedari <kedari.omkar.2001@gmail.com>
2025-08-04 21:03:32 +0530

    added the dockerfile

--- src/main/java/com/app/admin_backend_v1/repository/AmenityRepository.java
+++ src/main/java/com/app/admin_backend_v1/repository/AmenityRepository.java
@@ -2,8 +2,6 @@ package com.app.admin_backend_v1.repository;
 
 import com.app.admin_backend_v1.entity.Amenity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 
-@RepositoryRestController
 public interface AmenityRepository extends JpaRepository<Amenity, Integer> {
 }
--- src/main/java/com/app/admin_backend_v1/repository/CityRepository.java
+++ src/main/java/com/app/admin_backend_v1/repository/CityRepository.java
@@ -2,8 +2,7 @@ package com.app.admin_backend_v1.repository;
 
 import com.app.admin_backend_v1.entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 
-@RepositoryRestController
+
 public interface CityRepository extends JpaRepository<City, Integer> {
 }
